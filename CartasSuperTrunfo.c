dcaba299e13a377ac6d7fb141401f3f4acf1ffd8
Vitor <assuncaoponcianovitor@gmail.com>
2025-01-30 21:01:52 +0000

    comecei o desafio!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,36 @@
 //Teste larissa
 
 int main() {
+    int população, pontos;
+    char Estado[50], Código[50], Nome[50];
+    float Àrea, PIB;
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    printf("Digite o Código da carta: \n");
+    scanf("%s", &Código);
+
+    printf("Digite o nome: \n");
+    scanf ("%s", &nome);
+
+    printf("Digite a população da cidade: \n");
+    scanf("%d", &população);
+
+    printf("Digite a Àrea da cidade: \n");
+    scanf("%f", &Àrea);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%d", &PIB);
+
+    printf("Digite a quantidade de pontos turísticos da cidade: \n");
+    scanf("%d", %pontos);
+
     
+
+
+
+
+
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
